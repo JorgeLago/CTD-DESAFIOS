631f842781e2b917768b2c4e56a5d6522d6a3fbb
Jorge Lago <jorgelago92@gmail.com>
2023-10-09 23:17:01 -0300

    Inicio

---
+++ DESAFIO01.py
@@ -0,0 +1,11 @@
+# Faça o terminal responder ao usuário
+
+nome = input('Qual o seu nome? ')
+print('Olá, {}!'.format(nome))
+
+# Leia dois numeros e mostre a soma deles
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+soma = num1+num2
+
+print(' A soma dos numeros dá {} !'.format(soma))
