ccc7cc68dd209723b5162369cdbb007a16ecd5ca
Jorge Lago <jorgelago92@gmail.com>
2023-10-13 00:12:08 -0300

    Atualizando

--- DESAFIO05.py
+++ DESAFIO05.py
@@ -2,3 +2,16 @@
 salario=float(input('Digite seu salário: R$'))
 aumento=salario+(salario*0.15)
 print(f'O seu salário é R${salario} e passará a ganhar R${aumento} com um aumento de 15%.')
+
+# Escreva um programa que leia uma temperatura em ºc e converta diretamente para ºF.
+tempc=float(input(' Digite uma temperatura em ºC : '))
+tempf=(tempc*1.8)+32
+print(f'A temperatura fica {tempf:.2f}ºF')
+
+# Escreva um programa que leia a quantd de km percorridos por um carro alugado e a quantd de dias pelos quais foi alugado. Calcule o preço a pagar. R$60,00 por dia e R$0,15 pro km rodado.
+dias=float(input(' Digite a qntd de dias alugados: '))
+dias=dias*60
+km=float(input(' Digite qnts km rodados: '))
+km=km*0.15
+total=dias+km
+print(f'O total a pagar será R${total}')
---
+++ DESAFIO06.py
@@ -0,0 +1,7 @@
+import math
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+num=float(input('Digite um número real: '))
+num2=math.trunc(num)
+print(f'Seu numero é {num} e sua parte inteira é {num2}')
+
+#
