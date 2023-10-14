d218b86796fe0818f675930c481f4cfd0b114102
Jorge Lago <jorgelago92@gmail.com>
2023-10-14 14:52:29 -0300

    Desafio dia 07

--- DESAFIO06.py
+++ DESAFIO06.py
@@ -1,7 +1,8 @@
 import math
-#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
-num=float(input('Digite um número real: '))
-num2=math.trunc(num)
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+num = float(input('Digite um número real: '))
+num2 = math.trunc(num)
 print(f'Seu numero é {num} e sua parte inteira é {num2}')
 
-#Faça um programa que leia um numero
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com toda a letras maiúcula e minúsculas, Quantas letras ao todo(sem contar os espaços), Quantas letras tem o primeiro nome.
