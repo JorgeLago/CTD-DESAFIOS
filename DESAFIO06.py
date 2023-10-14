2a46bfb93ae66e3a48da4616cc3392898b60a1c4
Jorge Lago <jorgelago92@gmail.com>
2023-10-14 18:47:12 -0300

    Resolvido Desafio 06

--- DESAFIO06.py
+++ DESAFIO06.py
@@ -6,3 +6,13 @@ print(f'Seu numero é {num} e sua parte inteira é {num2}')
 
 # Crie um programa que leia o nome completo de uma pessoa e mostre:
 # O nome com toda a letras maiúcula e minúsculas, Quantas letras ao todo(sem contar os espaços), Quantas letras tem o primeiro nome.
+nome = input('Digite seu nome completo: ')
+maiusculas = nome.upper()
+minusculas = nome.lower()
+qntd = len(nome) - nome.count(' ')
+primeiro = nome.split()
+primeiro = len((primeiro[0]))
+print(maiusculas)
+print(minusculas)
+print(qntd)
+print(primeiro)
