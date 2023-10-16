02601a0cbe2051c6b55d506896cf6d877f162703
Jorge Lago <jorgelago92@gmail.com>
2023-10-15 21:02:13 -0300

    Desafio 09

--- DESAFIO08.py
+++ DESAFIO08.py
@@ -2,4 +2,11 @@
 nome=input('Digite eu nome completo: ').upper().strip()
 print('SILVA' in nome)
 
-#
+#Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+nome=str(input('Digite seu nome completo: ').upper().strip())
+qntd=nome.count('A')
+primeiro=nome.find('A')+1
+ultimo=nome.rfind('A')+1
+print(f'Seu nome tem {qntd} A.s !')
+print(f'O primeiro aparece na posição {primeiro}')
+print(f'O ultimo aparece na posição {ultimo}')
