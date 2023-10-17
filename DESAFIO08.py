2d270779102fb6d4ac197902f77d135c2a4805b3
Jorge Lago <jorgelago92@gmail.com>
2023-10-16 21:33:03 -0300

    novo desafio

--- DESAFIO08.py
+++ DESAFIO08.py
@@ -12,6 +12,9 @@ print(f'O primeiro aparece na posição {primeiro}')
 print(f'O ultimo aparece na posição {ultimo}')
 
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
-
+nome = str(input('Qual seu Nome? '))
+nome=" ".join(nome)
+primeironome = nome[0]
+ultimonome = nome[-1]
 print(f'O seu primeiro nome é: {primeironome}')
 print(f'O seu ultimo nome é: {ultimonome}')
