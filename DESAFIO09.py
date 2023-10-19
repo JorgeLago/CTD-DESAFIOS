c0afee1629434538a9703677b1f85103fec2869e
Jorge Lago <jorgelago92@gmail.com>
2023-10-18 22:55:26 -0300

    Radar de velocidade

--- DESAFIO09.py
+++ DESAFIO09.py
@@ -6,3 +6,11 @@ if numero == user:
     print('Parabéns, voce acertou !!')
 else:
     print('Sinto muito, voce errou!')
+
+# radar de velocidade
+velocidade=int(input('Qual foi a velocidade?: '))
+multa=(velocidade-80)*7
+if velocidade <= 80:
+    print('Você está dentro do limite permitido !')
+else:
+    print(f'Você passou com {velocidade} e está acima do limite, sua multa será de R${multa}!')
