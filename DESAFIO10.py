adf92501aef40950cf5143bb4264e87a6dc5cd68
Jorge Lago <jorgelago92@gmail.com>
2023-10-19 21:36:14 -0300

    Ultimos da aula de condicao

--- DESAFIO10.py
+++ DESAFIO10.py
@@ -5,4 +5,10 @@ if distance <= 200:
 else:
     print(f'A sua passagem custará R${distance*0.45}!!')
 
-#
+#Faça um programa que leia um ano e mostre se ele é BISSEXTO!.
+
+#Faça um programa que leia 3 numeros e mostre qua o maior e qual o menor.
+
+#Escreva um programa que leia um salario e mostre seu aumento. se for mais que 1250 tem aumento de 10%, se for menor igual 15%.
+
+#Faça um programa que leia 3 comprimentos de retas e diga se podem ser um triangulo ou nao.
